5b9886e1e1ca9cffe6e0f4a15cb5c21027296846
ShivangiShastri <71712021+ShivangiShastri@users.noreply.github.com>
2024-05-05 13:48:08 -0500

    Add files via upload

---
+++ Front_End/src/app/restaurant-admin/edit-restaurant/edit-restaurant.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-restaurant',
+  templateUrl: './edit-restaurant.component.html',
+  styles: [
+  ]
+})
+export class EditRestaurantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
